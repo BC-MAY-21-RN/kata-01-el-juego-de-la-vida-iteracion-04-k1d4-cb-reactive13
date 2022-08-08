94d4be799c457eead1e481dab343f83cd532a63b
Jose Herrera <jherrera892016@gmail.com>
2022-08-08 17:48:05 -0500

    Scripts Antonioo

--- index.js
+++ index.js
@@ -1,36 +1,62 @@
-const Matrix = require("./models/matrix");
+const Matriz = require("./models/matrix.js");
+/*
+Pasos para correr esta cosa
+1- Instanciar un objeto de matrix.js
+2- Crear la matriz
+3- LLenar la matriz
+4- Dos opciones aqui
+    |----------------------------------------------------------------------------------------|
+   | 4A -> Para imprimir solo dos generaciones.                                            |
+   | 4B -> Para imprimir x numero de generaciones                                          |
+   | --------------------------------------------------------------------------------------|
+   | 4A- let primeraGen = matriz.llenarMatriz();                                           |
+   |     matriz.imprimirMatriz(primeraGen);                                                |
+   |     let segundaGen = matriz.siguienteGeneracion(primeraGen);                          |
+   |     matriz.imprimirMatriz(segundaGen);                                                |
+   | --------------------------------------------------------------------------------------|
+   | 4B-                                                                                   |
+   |     let primerGen = matriz.llenarMatriz();                                            |
+   |     matriz.imprimirVariasGeneraciones(numDeGeneraciones, primerGen);                  |
+   | --------------------------------------------------------------------------------------|
+*/
+let matriz = new Matriz(8, 8); // Paso 1
+matriz.crearMatriz(); // Paso 2
+let primerGen = matriz.llenarMatriz(); // Paso 3
+matriz.imprimirVariasGeneraciones(8, primerGen);
 
-let matriz = new Matrix(6, 6);
+// const Matrix = require("./models/matrix");
 
-matriz.crearMatriz();
-matriz.llenarMatriz();
-matriz.agregarCelulasAleatorias();
-// let celulasVivas = [[1,4],[2,3],[2,4]]
-// matriz.fillLiveCells(celulasVivas)
-// console.log("________________")
-// matriz.siguienteGeneracion();
-console.table(matriz.imprimirMatriz());
-/**
- * 
- * constructor(x, y) {
-this.x = x;
-this.y = y;
-this.table = this.initializeBoard();
-}
+// let matriz = new Matrix(6, 6);
 
-initializeBoard() {
-let array1 = [];
-let array2 = [];
+// matriz.crearMatriz();
+// matriz.llenarMatriz();
+// matriz.agregarCelulasAleatorias();
+// // let celulasVivas = [[1,4],[2,3],[2,4]]
+// // matriz.fillLiveCells(celulasVivas)
+// // console.log("________________")
+// // matriz.siguienteGeneracion();
+// console.table(matriz.imprimirMatriz());
+// /**
+//  *
+//  * constructor(x, y) {
+// this.x = x;
+// this.y = y;
+// this.table = this.initializeBoard();
+// }
 
-for (let i = 0; i < this.y; i++) {
-for (let j = 0; j < this.x; j++) {
-let x = Math.floor(Math.random() * 2);
-array2.push(x);
-}
-array1.push(array2);
-array2 = [];
-}
-this.table = array1;
-return this.table;
-}
- */
+// initializeBoard() {
+// let array1 = [];
+// let array2 = [];
+
+// for (let i = 0; i < this.y; i++) {
+// for (let j = 0; j < this.x; j++) {
+// let x = Math.floor(Math.random() * 2);
+// array2.push(x);
+// }
+// array1.push(array2);
+// array2 = [];
+// }
+// this.table = array1;
+// return this.table;
+// }
+//  */
--- models/matrix.js
+++ models/matrix.js
@@ -5,7 +5,6 @@ class Matriz {
     this.matriz = [];
   }
 
-  // Creamos matriz de x, y
   crearMatriz() {
     for (let i = 0; i < this.rows; i++) {
       this.matriz[i] = new Array(this.columns);
@@ -13,28 +12,10 @@ class Matriz {
     return this.matriz;
   }
 
-  // Llenamos matriz de puntitos
   llenarMatriz() {
     for (let i = 0; i < this.rows; i++) {
       for (let j = 0; j < this.columns; j++) {
-        this.matriz[i][j] = ".";
-      }
-    }
-    return this.matriz;
-  }
-
-  llenarMatrizDeCerosYUnos() {
-    for (let i = 0; i < this.rows; i++) {
-      for (let j = 0; j < this.columns; j++) {
         this.matriz[i][j] = Math.floor(Math.random() * 2);
-      }
-    }
-    return this.matriz;
-  }
-
-  agregarCelulasAleatorias() {
-    for (let i = 0; i < this.rows; i++) {
-      for (let j = 0; j < this.columns; j++) {
         this.matriz[i][j] === 1
           ? (this.matriz[i][j] = "*")
           : (this.matriz[i][j] = ".");
@@ -43,93 +24,210 @@ class Matriz {
     return this.matriz;
   }
 
-  imprimirMatriz() {
-    for (let i = 0; i < this.matriz.length; i++) {
-      let line = "";
-      for (let j = 0; j < this.matriz[i].length; j++) {
-        line += this.matriz[i][j] + " ";
-      }
-      console.log(line);
-    }
-    return "";
-  }
-
-  // coordenadas = [ [x,y] , [x,y] , [x,y]  ]
-
-  fillLiveCells(cells) {
-    cells.forEach((coordenada) => {
-      this.matriz[coordenada[0]][coordenada[1]] = "*";
-    });
-
-    this.imprimirMatriz();
-    return this.matriz;
-  }
-
-  siguienteGeneracion() {
-    for (let y = 1; y < this.columns - 1; y++) {
-      for (let x = 1; x < this.rows - 1; x++) {
-        let num = this.countNeighbours(x, y);
-        if (this.matriz[x][y] == ".") {
-          if (num == 3) this.matriz[x][y] = "*";
-        } else {
-          if (num == 3 || num == 2) {
-            this.matriz[x][y] = "*";
-          } else {
-            this.matriz[x][y] = ".";
+  siguienteGeneracion(primeraGen) {
+    // Generamos copia de la matriz, donde pondremos los nuevos valores de las celulas
+    let sigGen = primeraGen.map((arr) => [...arr]);
+    // Recorremos matriz original
+    for (let i = 0; i < primeraGen.length; i++) {
+      for (let j = 0; j < primeraGen[i].length; j++) {
+        // Variable para aplicar los vecinos
+        let celula = primeraGen[i][j];
+        // Creamos variable para contar vecinos
+        let numDeVecinos = 0;
+        // Recorremos los vecinos de la celula en cuestion
+        for (let row = -1; row < 2; row++) {
+          for (let col = -1; col < 2; col++) {
+            // Si estamos sobre la celula de la que queremos contar los vecinos, la brincamos.
+            if (row === 0 && col === 0) {
+              continue;
+            }
+            // Definimos variables para la posicion del vecino
+            let vecinoX = i + row;
+            let vecinoY = j + col;
+            // Si estamos fuera de la matriz no entramos al if.
+            if (
+              vecinoX >= 0 &&
+              vecinoY >= 0 &&
+              vecinoX < primeraGen.length &&
+              vecinoY < primeraGen[i].length
+            ) {
+              if (primeraGen[vecinoX][vecinoY] === "*") {
+                numDeVecinos++;
+              }
+            }
           }
+        }
+        /* Cuando salimos del ciclo de conteo de vecinos, tenemos el numero de vecinos
+        para la celula en posicion [i][j]
+        es aqui cuando aplicamos las reglas de la vida
+        */
+        if (celula === "*" && numDeVecinos < 2) {
+          sigGen[i][j] = ".";
+        } else if (celula === "*" && numDeVecinos > 3) {
+          sigGen[i][j] = ".";
+        } else if (celula === "." && numDeVecinos === 3) {
+          sigGen[i][j] = "*";
         }
       }
     }
+    return sigGen;
+  }
 
-    this.imprimirMatriz();
+  imprimirVariasGeneraciones(numeroDeGeneraciones, generacion) {
+    for (let i = 0; i <= numeroDeGeneraciones; i++) {
+      console.log("Generacion: " + i);
+      this.imprimirMatriz(generacion);
+      generacion = this.siguienteGeneracion(generacion);
+      console.log("--------------------");
+    }
+  }
+
+  imprimirMatriz(matriz) {
+    for (let i = 0; i < matriz.length; i++) {
+      let line = "";
+      for (let j = 0; j < matriz[i].length; j++) {
+        line += matriz[i][j] + " ";
+      }
+      console.log(line);
+    }
+    // let nextGen = this.crearMatriz(rows, columns);
     return "";
   }
-
-  /*countNeighbours(x, y) {
-    var neighbours = 0;
-
-    for (var i = -1; i < 1; i++) {
-      for (var j = -1; j < 1; j++) {
-        if (this.matriz[x + i][y + j] == "*" && (x != 0 && y != 0)) 
-            neighbours++;
-      }
-    }
-
-    return neighbours;
-  }
-  */
-
-  countNeighbours(x, y) {
-    let neighbours = 0;
-    neighbours += this.contarLados(x - 1, y - 1);
-    neighbours += this.contarMedios(x, y);
-    neighbours += this.contarLados(x - 1, y + 1);
-
-    return neighbours;
-  }
-
-  contarLados(x, y) {
-    let count = 0;
-    for (let i = 0; i < 3; i++) {
-      if (this.matriz[x][y] == "*") {
-        count++;
-      }
-      x++;
-    }
-    return count;
-  }
-
-  contarMedios(x, y) {
-    let count = 0;
-    if (this.matriz[x - 1][y] == "*") {
-      count++;
-    }
-
-    if (this.matriz[x + 1][y] == "*") {
-      count++;
-    }
-    return count;
-  }
 }
-
 module.exports = Matriz;
+
+// class Matriz {
+//   constructor(columns, rows) {
+//     this.columns = columns;
+//     this.rows = rows;
+//     this.matriz = [];
+//   }
+
+//   // Creamos matriz de x, y
+//   crearMatriz() {
+//     for (let i = 0; i < this.rows; i++) {
+//       this.matriz[i] = new Array(this.columns);
+//     }
+//     return this.matriz;
+//   }
+
+//   // Llenamos matriz de puntitos
+//   llenarMatriz() {
+//     for (let i = 0; i < this.rows; i++) {
+//       for (let j = 0; j < this.columns; j++) {
+//         this.matriz[i][j] = ".";
+//       }
+//     }
+//     return this.matriz;
+//   }
+
+//   llenarMatrizDeCerosYUnos() {
+//     for (let i = 0; i < this.rows; i++) {
+//       for (let j = 0; j < this.columns; j++) {
+//         this.matriz[i][j] = Math.floor(Math.random() * 2);
+//       }
+//     }
+//     return this.matriz;
+//   }
+
+//   agregarCelulasAleatorias() {
+//     for (let i = 0; i < this.rows; i++) {
+//       for (let j = 0; j < this.columns; j++) {
+//         this.matriz[i][j] === 1
+//           ? (this.matriz[i][j] = "*")
+//           : (this.matriz[i][j] = ".");
+//       }
+//     }
+//     return this.matriz;
+//   }
+
+//   imprimirMatriz() {
+//     for (let i = 0; i < this.matriz.length; i++) {
+//       let line = "";
+//       for (let j = 0; j < this.matriz[i].length; j++) {
+//         line += this.matriz[i][j] + " ";
+//       }
+//       console.log(line);
+//     }
+//     return "";
+//   }
+
+//   // coordenadas = [ [x,y] , [x,y] , [x,y]  ]
+
+//   fillLiveCells(cells) {
+//     cells.forEach((coordenada) => {
+//       this.matriz[coordenada[0]][coordenada[1]] = "*";
+//     });
+
+//     this.imprimirMatriz();
+//     return this.matriz;
+//   }
+
+//   siguienteGeneracion() {
+//     for (let y = 1; y < this.columns - 1; y++) {
+//       for (let x = 1; x < this.rows - 1; x++) {
+//         let num = this.countNeighbours(x, y);
+//         if (this.matriz[x][y] == ".") {
+//           if (num == 3) this.matriz[x][y] = "*";
+//         } else {
+//           if (num == 3 || num == 2) {
+//             this.matriz[x][y] = "*";
+//           } else {
+//             this.matriz[x][y] = ".";
+//           }
+//         }
+//       }
+//     }
+
+//     this.imprimirMatriz();
+//     return "";
+//   }
+
+//   /*countNeighbours(x, y) {
+//     var neighbours = 0;
+
+//     for (var i = -1; i < 1; i++) {
+//       for (var j = -1; j < 1; j++) {
+//         if (this.matriz[x + i][y + j] == "*" && (x != 0 && y != 0))
+//             neighbours++;
+//       }
+//     }
+
+//     return neighbours;
+//   }
+//   */
+
+//   countNeighbours(x, y) {
+//     let neighbours = 0;
+//     neighbours += this.contarLados(x - 1, y - 1);
+//     neighbours += this.contarMedios(x, y);
+//     neighbours += this.contarLados(x - 1, y + 1);
+
+//     return neighbours;
+//   }
+
+//   contarLados(x, y) {
+//     let count = 0;
+//     for (let i = 0; i < 3; i++) {
+//       if (this.matriz[x][y] == "*") {
+//         count++;
+//       }
+//       x++;
+//     }
+//     return count;
+//   }
+
+//   contarMedios(x, y) {
+//     let count = 0;
+//     if (this.matriz[x - 1][y] == "*") {
+//       count++;
+//     }
+
+//     if (this.matriz[x + 1][y] == "*") {
+//       count++;
+//     }
+//     return count;
+//   }
+// }
+
+// module.exports = Matriz;
